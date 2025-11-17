26e2b57c4be5f3347e81b4b11e31eab3e558df90
CamiriJiang <jmingling601@gmail.com>
2025-11-18 03:19:03 +0800

    Delete mychatmachine directory

--- mychatmachine/基于langchain的代码生成系统.py
+++
@@ -1,8 +0,0 @@
-#需求：prompt:请为以下功能生成python代码，功能：计算两个数的最大公约数
-
-from langchain_community.llms import Tongyi
-
-llm = Tongyi(model = 'qwen-max')
-prompt = '请为计算两个数最大公约数这个功能生成python代码'
-result = llm.invoke(prompt)
-print(f'代码生成如下：\n{result}')
